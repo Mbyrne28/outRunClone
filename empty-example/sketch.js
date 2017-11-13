537b6aa8b8eea896376fe36275a5567be96e807b
Matt Byrne <Matt.byrne@yahoo.co.uk>
2017-11-13 07:51:24 +0000

    add comments

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -10,12 +10,11 @@ function setup() {
   background("lightblue");
 
   	for (var y=0;y<(SCREENHEIGHT/2)/8;y++){
-  		//console.log(y,y/SCREENHEIGHT )
+  		//Generate Road segments
   		roads[y] = new Road(0,(SCREENHEIGHT/2)+y*8,SCREENWIDTH,8);
-  		
+  		//Apply Scaling
   		roads[y].roadScale = (y/SCREENHEIGHT * 10) +0.05
-
-
+  		//Show road
   		roads[y].show()
 	}
 
@@ -25,7 +24,7 @@ function draw() {
   // put drawing code here
   	console.log("grass")
   	for (var i=0;i<roads.length;i++){
-  		//roads[i].show()
+  		//Apply 3D Colours
   		var grass = Math.abs(Math.sin(20*Math.pow((roads.length/i),3)))
   		if(grass>0.5){
   			roads[i].grassColour="lightgreen"
